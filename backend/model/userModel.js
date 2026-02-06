abb9c3e556e695cea7e6e51535c228a9de8e495f
yogeshwaran99 <itsyogeshwaran@gmail.com>
2026-02-06 15:26:55 +0530

    added basic user controller

---
+++ backend/model/userModel.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const schema =  new mongoose.Schema({
+    username : {type:String,required:true},
+    password : {type:String,required:true},
+    role : {type:String,required:true}
+});
+
+export default mongoose.model('userSchema',schema);
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
-const express = require ('express');
-const mongoose = require ('mongoose');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv');
+import dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+import connectDB from "./config/db.js";
+import userRouter from "./routes/userRouter.js";
+
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT||3000;
 
+app.use(cors());
 app.use(express.json());
-
+app.use('/api/user',userRouter);
 connectDB();
 
-PORT = process.env.PORT;
 
 app.listen(PORT,()=>{
     console.log(`server running succesfully on port http://localhost:${PORT}`);
